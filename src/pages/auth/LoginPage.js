21a89f2a3c2db3d8729fe287c94746e4e1a98bc6
ranky akab <rankyakab@gmail.com>
2023-03-07 10:49:31 +0100

    more cleaning up

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -145,53 +145,6 @@ export default function LoginPage() {
 
             {/* Form */}
 
-            <FormControl sx={{ pt: 3 }}>
-
-             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-              <Stack sx={{ mt: 2 }}>
-                <FormLabel id="email" sx={{ color: 'black', pb: 1 }}>
-                  Email address
-                </FormLabel>
-                <TextField
-                  id="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  ref={userRef}
-                  value={email}
-                  placeholder="Email address"
-                  required
-                />
-              </Stack>
-
-              <Stack sx={{ mt: 3 }}>
-                <FormLabel id="password" sx={{ color: 'black', pb: 1 }}>
-                  Password
-                </FormLabel>
-                <TextField
-                  id="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  required
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                          <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Stack>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: '12px' }}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Checkbox {...label} />
-                  <Typography>Remember me</Typography>
-                </div>
-                <Typography>I forgot my password</Typography>
-              </Box>
-  
-            </FormControl>
           </Box>
         </Grid>
         <Grid
