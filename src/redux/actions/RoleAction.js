8252997d74104a15d109f3be35a4965793d52056
ranky akab <rankyakab@gmail.com>
2023-03-21 14:05:58 +0100

    woking on memo

--- src/redux/actions/RoleAction.js
+++ src/redux/actions/RoleAction.js
@@ -44,9 +44,9 @@ export const getRoleById = (id) => async (dispatch) => {
       needToken: true,
     });
 
-    console.log("please work",res);
+    console.log("please work",res.data.role[0]);
 
-    localStorage.setItem('role', JSON.stringify(res?.data?.role[0][0]));
+    localStorage.setItem('role', JSON.stringify(res?.data?.role[0]));
 
     if (res.status === 200 || res.status === 201) {
       dispatch(setIsLoading(false));
