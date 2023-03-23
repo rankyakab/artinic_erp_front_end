3585370221da1767cc311350a5a5f36818a78e58
ranky akab <rankyakab@gmail.com>
2023-03-23 05:23:25 +0100

    working on fxing the blank screen read

--- src/components/nav-section/vertical/NavSectionVertical.js
+++ src/components/nav-section/vertical/NavSectionVertical.js
@@ -1,15 +1,12 @@
-import React, {  useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 // @mui
 import { List, Stack } from '@mui/material';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 // locales
 import { useLocales } from '../../../locales';
 //
 import { StyledSubheader } from './styles';
 import NavList from './NavList';
-import { getRoleById } from '../../../redux/actions/RoleAction';
 
 // ----------------------------------------------------------------------
 
@@ -20,74 +17,36 @@ NavSectionVertical.propTypes = {
 
 export default function NavSectionVertical({ data, sx, ...other }) {
   const { translate } = useLocales();
-    const dispatch = useDispatch();
-   const user = JSON.parse(localStorage.getItem('user'));
- const { role } = useSelector((state) => state.role);
-    //  const user = localStorage.getItem('user');
-         
- // const [userRole, setUserRole] = useState({})
+  const user = JSON.parse(localStorage.getItem('user'));
 
-useEffect(() => {
-    dispatch(getRoleById(user?.user.role));
-    
-    
+  console.log(user);
 
-  }, []);
+  const { processes } = useSelector((state) => state?.process);
 
+  console.log(processes);
 
-    
-  const userRole = JSON.parse(localStorage.getItem('role'));
-// const privilage = userRole.privilege.map(item => item.processId)
-let  prev ="";
-prev = userRole?.privilege.map(event=>event.processId)&&userRole?.privilege.map(event=>event.processId);
-  useEffect(() => {
-    
-      
-    console.log("this is your ysoueer local storagerole",prev);
-  
-    console.log("this is your ysoueer local storagerole",userRole?.privilege);
-  //  console.log("this is your ysoueer role from dispatch",role);
-   //  console.log("this is your ysoueer user from localstorage",user);
-  }, [dispatch]);
+  const { roles } = useSelector((state) => state?.role);
 
+  console.log(roles);
 
-      // user.role
+  const userRole = roles.filter((role) => role?._id === user?.user?.role);
 
- //  console.log(user);
- 
- // const { processes } = useSelector((state) => state?.process);
-
-  // console.log(processes);
-
-  // const { roles } = useSelector((state) => state?.role);
-
-  // console.log(roles);
-
- // const userRole = roles.filter((role) => role?._id === user?.user?.role);
-
-  // console.log(userRole);
+  console.log(userRole);
 
   return (
     <Stack sx={sx} {...other}>
       {data.map((group) => {
         const key = group.subheader || group.items[0].title;
 
-       // console.log(group.subheader);
-       // list.process &&
-       let count = 0;
+        console.log(group.subheader);
+
         return (
           <List key={key} disablePadding sx={{ px: 2 }}>
             {group.subheader && <StyledSubheader disableSticky>{translate(group.subheader)}</StyledSubheader>}
 
-            {group.items.map(
-              (list) =>{
-                count+=1;
-              //   console.log("this is the link ", count);
-            //  return  prev.includes(list.process)&&(<><NavList key={list.title + list.path} data={list} depth={1} hasChild={!!list.children} />{console.log('hello process', list.process)} {console.log('hello process list', list)}</>)
-                 return  (<NavList key={list.title + list.path} data={list} depth={1} hasChild={!!list.children} />)
-              
-            }
-            )}
+            {group.items.map((list) => (
+              <NavList key={list.title + list.path} data={list} depth={1} hasChild={!!list.children} />
+            ))}
           </List>
         );
       })}
